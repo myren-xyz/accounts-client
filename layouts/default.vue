d02067074a1bf5bd7009ae292ccaed0e68df3386
blackestwhite <pesaregoal@gmail.com>
2023-03-13 13:59:49 +0330

    Add basic skeleton

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <header class="bg-black text-white py-4 flex justify-center">
+            <h1 class="mulish tracking-[.75rem]"> ACCOUNTS </h1>
+        </header>
+        
+        <nuxt />
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container mx-auto">
+        <p> hello world </p>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+    name: 'IndexPage'
+})
